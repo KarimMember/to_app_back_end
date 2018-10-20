7e63f8f2e160170d78a4ca4350424418290d32f9
Karim Member <fazlekarim.member1@gmail.com>
2018-10-20 19:47:01 +0000

    TODOLIST

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,18 +1,22 @@
 class TodoController < ApplicationController
+     $todos = ["DO Homework", "Help Mom", "Play Fortnite"]
+     
+     
     def index
        
     end
     def show
         todo_id = params[:id]
-        if todo_id == '1'
-            @todo_description = "Do homework"
+        @todo_description = $todos[todo_id.to_i]
+    #     if todo_id == '1'
+    #         @todo_description = "Do homework"
         
-        elsif todo_id == '2'
-            @todo_description = "Help mom"
+    #     elsif todo_id == '2'
+    #         @todo_description = "Help mom"
             
-        elsif todo_id == '3'
-            @todo_description = "Play fortnite"
-     end 
+    #     elsif todo_id == '3'
+    #         @todo_description = "Play fortnite"
+    #  end 
     end
 end
 
