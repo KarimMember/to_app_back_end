d1684979f622c1157378f069e6929e042de25d55
Karim Member <fazlekarim.member1@gmail.com>
2018-10-20 19:22:59 +0000

    TodoList

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,4 +2,17 @@ class TodoController < ApplicationController
     def index
        
     end
+    def show
+        todo_id = params[:id]
+        if todo_id == '1'
+            @todo_description = "Do homework"
+        
+        elsif todo_id == '2'
+            @todo_description = "Help mom"
+            
+        elsif todo_id == '3'
+            @todo_description = "Play fortnite"
+     end 
+    end
 end
+
