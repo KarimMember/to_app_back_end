49af096814435e5aa8b5553b6a4bcbcf2412320c
Karim Member <fazlekarim.member1@gmail.com>
2018-10-21 19:04:24 +0000

    To Do list final

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,7 +7,10 @@ class TodoController < ApplicationController
     end
     def show
         todo_id = params[:id]
-        @todo_description = $todos[todo_id.to_i]
+        #get the row with that id
+        @todo = Todo.find_by_id(params[:id])
+        
+        #Set the descriotion
     #     if todo_id == '1'
     #         @todo_description = "Do homework"
         
@@ -16,7 +19,25 @@ class TodoController < ApplicationController
             
     #     elsif todo_id == '3'
     #         @todo_description = "Play fortnite"
-    #  end 
+    end 
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.added_ = DateTime.now
+        t.save
+        
+        redirect_to "/todo/show/#{t.id}"
+      
+    end
+    
+    def edit
+        @todo=Todo.find_by_id(params[:id])
+    end
+    
+    def update
+        t= Todo.find_by_id(params[:id])
+        t.description = params['description']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
     end
 end
-
